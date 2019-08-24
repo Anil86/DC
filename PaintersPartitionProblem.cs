f980c36fbb543c4ad5e1217c1e9bd26825b5179c
Anil Kumar <anil.apex@outlook.com>
2019-08-24 13:31:19 +0530

    Solve "Painter Partition Problem" using DP.
    Related Work Items: #800

--- PaintersPartitionProblem.cs
+++ PaintersPartitionProblem.cs
@@ -5,27 +5,29 @@ namespace DC
 {
     public class PaintersPartitionProblem
     {
-        private static int _count;
         private int MinimizeMaxWork(int noOfPainters, int[] boards)
         {
-            return MinimizeMaxWork(boards.Length - 1, noOfPainters);
+            int[,] dp = new int[boards.Length, noOfPainters + 1];
+
+            return MinimizeMaxWorkLocal(boards.Length - 1, noOfPainters);
 
 
 
-            int MinimizeMaxWork(int noOfBoards, int painters)
+            int MinimizeMaxWorkLocal(int noOfBoards, int painters)
             {
-                _count++;
                 // Solve small sub-problems
-                if (painters == 1) return SumWork(0, noOfBoards);   // No.of painters = 1
+                if (painters == 1) return dp[noOfBoards, 1] = SumWork(0, noOfBoards); // No.of painters = 1
 
-                if (noOfBoards == 0) return boards[0];   // No.of boards = 1
+                if (noOfBoards == 0) return dp[0, painters] = boards[0]; // No.of boards = 1
 
 
                 // Divide
                 int minimizedMaxWork = int.MaxValue;
                 for (int i = 0; i < noOfBoards; i++)
                 {
-                    int prevPaintersMax = MinimizeMaxWork(i, painters - 1);
+                    int prevPaintersMax = dp[i, painters - 1] == 0
+                        ? dp[i, painters - 1] = MinimizeMaxWorkLocal(i, painters - 1)
+                        : dp[i, painters - 1];
                     int lastPainterSum = SumWork(i + 1, noOfBoards);
 
 
@@ -34,7 +36,7 @@ namespace DC
                     minimizedMaxWork = Math.Min(currentMaxWork, minimizedMaxWork);
                 }
 
-                return minimizedMaxWork;
+                return dp[noOfBoards, painters] = minimizedMaxWork;
             }
 
 
@@ -49,7 +51,6 @@ namespace DC
         }
 
 
-
         internal static void Work()
         {
             //int noOfPainters = 2;
@@ -59,16 +60,24 @@ namespace DC
             //int noOfPainters = 3;
             //int[] boards = { 2, 1, 3, 4, 9, 8 };   // Ans: 10
 
-            int noOfPainters = 14;
-            int[] boards =
-            {
-                189, 107, 444, 400, 84, 270, 225, 334, 410, 433, 249, 193, 487, 312, 493, 430, 422, 208, 90, 245, 337,
-                234, 168, 360
-            };
+            //int noOfPainters = 14;
+            //int[] boards =
+            //{
+            //    189, 107, 444, 400, 84, 270, 225, 334, 410, 433, 249, 193, 487, 312, 493, 430, 422, 208, 90, 245, 337,
+            //    234, 168, 360
+            //};
             // Ans: 740   // Calls: 6690448
 
+            int noOfPainters = 26;
+            int[] boards =
+            {
+                274, 465, 130, 135, 254, 45, 70, 122, 149, 95, 453, 65, 392, 331, 316, 484, 372, 339, 45, 46, 31, 167,
+                351, 415, 387, 275, 355, 440, 290, 462, 436, 416, 279, 66, 403, 33, 464, 473, 8, 113, 420, 461, 30, 312
+            };
+            // Ans: 647
+
             int fairMaxWork = new PaintersPartitionProblem().MinimizeMaxWork(noOfPainters, boards);
-            WriteLine(fairMaxWork); WriteLine($"Count: {_count}");
+            WriteLine(fairMaxWork);
         }
     }
 }
